a62b03d0ce86a16f313b28cd4af25d5a09659a2a
shermp <14854761+shermp@users.noreply.github.com>
2021-12-16 20:19:31 +1300

    Fix implicit warning

--- src/sp_parser.c
+++ src/sp_parser.c
@@ -5,6 +5,7 @@
  */
 
 #include <limits.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
--- src/structpack.c
+++ src/structpack.c
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
