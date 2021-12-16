a8a4573ac67e619dc7855f93682f49a5821b2b82
shermp <14854761+shermp@users.noreply.github.com>
2021-12-17 10:55:53 +1300

    Add dedicated test header

--- test/sp_parser_test.c
+++ test/sp_parser_test.c
@@ -4,44 +4,37 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include <stdio.h>
+#include "sp_test.h"
 
 #include "../src/sp_parser.h"
 
-static int rv = 0;
-
-#define SP_TEST_ASSERT(cond, str) if ((cond)) {        \
-        printf("[PASS] %s\n", (str));                  \
-    } else {                                           \
-        printf("[FAIL] %s\n", (str));                  \
-        rv = 1;                                        \
-    }                                                  
+static int rv = 0;                                   
 
 int main(void) {
     const char *fmt_str;
     /* Good format string */
     fmt_str = ">4s h i 4(4B Q)";
-    SP_TEST_ASSERT(validate_format_str(fmt_str) == SP_OK, "parse good format string");
+    SP_TEST_ASSERT(rv, validate_format_str(fmt_str) == SP_OK, "parse good format string");
 
     /* Invalid chars */
     fmt_str = ">4s h i 4(4P Q)";
-    SP_TEST_ASSERT(validate_format_str(fmt_str) != SP_OK, "parse invalid character");
+    SP_TEST_ASSERT(rv, validate_format_str(fmt_str) != SP_OK, "parse invalid character");
     
     /* Check invalid endian char location */
     fmt_str = "4s >h i 4(4B Q)";
-    SP_TEST_ASSERT(validate_format_str(fmt_str) != SP_OK, "parse invalid endian location");
+    SP_TEST_ASSERT(rv, validate_format_str(fmt_str) != SP_OK, "parse invalid endian location");
     
     /* Test beginning with '(' */
     fmt_str = "(4s) h i 4(4B Q)";
-    SP_TEST_ASSERT(validate_format_str(fmt_str) != SP_OK, "parse invalid opening '('");
+    SP_TEST_ASSERT(rv, validate_format_str(fmt_str) != SP_OK, "parse invalid opening '('");
     
     /* Test invalid ending character */
     fmt_str = ">4s h i 4(4B Q)]";
-    SP_TEST_ASSERT(validate_format_str(fmt_str) != SP_OK, "pare invalid end character");
+    SP_TEST_ASSERT(rv, validate_format_str(fmt_str) != SP_OK, "pare invalid end character");
     
     /* Test max depth detection */
     fmt_str = ">4s h i 4(4B Q 4(4B Q 4(4B Q 4(4B Q 4(4B Q 4(4B Q 4(4B Q 4(4B Q 4(4B Q 4(4B Q 4(4B Q))))))))))";
-    SP_TEST_ASSERT(validate_format_str(fmt_str) != SP_OK, "parse beyond max group depth");
+    SP_TEST_ASSERT(rv, validate_format_str(fmt_str) != SP_OK, "parse beyond max group depth");
     
     return rv;
 }
---
+++ test/sp_test.h
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2019-2021 John Smith
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef SP_TEST_H
+#define SP_TEST_H
+
+#include <stdio.h>
+
+#define SP_TEST_ASSERT(ret, cond, str) if ((cond)) {        \
+        printf("[PASS] %s\n", (str));                  \
+    } else {                                           \
+        printf("[FAIL] %s\n", (str));                  \
+        rv = 1;                                        \
+    }             
+
+#endif
