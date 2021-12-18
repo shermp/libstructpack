31c9b577925e682bb1587d3c0e367f30d19c01f2
shermp <14854761+shermp@users.noreply.github.com>
2021-12-19 09:30:11 +1300

    Increase SP_ADD_STRUCT_OFFSET max args tp 16

--- include/sp_struct_offsets.h
+++ include/sp_struct_offsets.h
@@ -9,19 +9,8 @@
 
 #include <stddef.h>
 
-/* The following macros help make populating an offset list less tedious */
+/* The following macro help make populating an offset list less tedious */
 
-/*!
- * \brief Assign struct member offset(s) to an offset array
- *
- * The following macros use offsetof to add the offsets of <n> struct members
- * to an offset array for use with sp_unpack_bin_offset and sp_pack_bin_offset.
- * 
- * \param arr : array of type size_t arr[]
- * \param index : start index in arr to use
- * \param type : struct type, eg: 'struct somestruct'
- * \param mn : Struct member to obtain offset for. There can be up to 10, depending on macro
- */
 #define SP_OFFSET1(arr, index, type, m1) (arr)[(index)] = offsetof(type, m1)
 
 #define SP_OFFSET2(arr, index, type, m1, m2) \
@@ -60,9 +49,45 @@
     SP_OFFSET9((arr), (index), type, m1, m2, m3, m4, m5, m6, m7, m8, m9); \
     SP_OFFSET1((arr), (index) + 9, type, m10)
 
+#define SP_OFFSET11(arr, index, type, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11) \
+    SP_OFFSET10((arr), (index), type, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10); \
+    SP_OFFSET1((arr), (index) + 10, type, m11)
+
+#define SP_OFFSET12(arr, index, type, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12) \
+    SP_OFFSET11((arr), (index), type, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11); \
+    SP_OFFSET1((arr), (index) + 11, type, m12)
+
+#define SP_OFFSET13(arr, index, type, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13) \
+    SP_OFFSET12((arr), (index), type, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12); \
+    SP_OFFSET1((arr), (index) + 12, type, m13)
+
+#define SP_OFFSET14(arr, index, type, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14) \
+    SP_OFFSET13((arr), (index), type, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13); \
+    SP_OFFSET1((arr), (index) + 13, type, m14)
+
+#define SP_OFFSET15(arr, index, type, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15) \
+    SP_OFFSET14((arr), (index), type, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14); \
+    SP_OFFSET1((arr), (index) + 14, type, m15)
+
+#define SP_OFFSET16(arr, index, type, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16) \
+    SP_OFFSET15((arr), (index), type, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15); \
+    SP_OFFSET1((arr), (index) + 15, type, m16)
+
 // Based on the example provided at https://stackoverflow.com/a/69945225/16776990
 #define EXPAND(x) x
-#define GET_MACRO(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, NAME, ...) NAME
-#define SP_ADD_STRUCT_OFFSET(arr, index, type, ...) EXPAND(GET_MACRO(__VA_ARGS__, SP_OFFSET10, SP_OFFSET9, SP_OFFSET8, SP_OFFSET7, SP_OFFSET6, SP_OFFSET5, SP_OFFSET4, SP_OFFSET3, SP_OFFSET2, SP_OFFSET1)(arr, index, type, __VA_ARGS__))
+#define GET_MACRO(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, NAME, ...) NAME
+
+/*!
+ * \brief Assign struct member offset(s) to an offset array
+ *
+ * This variadic macro use offsetof to add the offsets of up to 16 struct members
+ * to an offset array for use with sp_unpack_bin_offset and sp_pack_bin_offset.
+ * 
+ * \param arr : array of type size_t arr[]
+ * \param index : start index in arr to use
+ * \param type : struct type, eg: 'struct somestruct'
+ * \param ... : Struct members to obtain offset for. There can be up to 16
+ */
+#define SP_ADD_STRUCT_OFFSET(arr, index, type, ...) EXPAND(GET_MACRO(__VA_ARGS__, SP_OFFSET16, SP_OFFSET15, SP_OFFSET14, SP_OFFSET13, SP_OFFSET12, SP_OFFSET11, SP_OFFSET10, SP_OFFSET9, SP_OFFSET8, SP_OFFSET7, SP_OFFSET6, SP_OFFSET5, SP_OFFSET4, SP_OFFSET3, SP_OFFSET2, SP_OFFSET1)(arr, index, type, __VA_ARGS__))
 
 #endif // SP_STRUCT_OFFSETS_H
